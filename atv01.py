97d9e04237a6db0610285bc2f943a4f7331f60f6
emanuel378 <emanuelmvieira10@gmail.com>
2024-09-04 16:01:33 -0300

    soma numeros

---
+++ atv01.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+
+n1=int(input('Digite o primeiro numero'))
+n2 =int(input("Digite o primeiro numero2"))
+sum =n1 + n2
+print(f"a soma de {n1} + {n2} e = {sum}")
